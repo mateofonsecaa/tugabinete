5d7a0d91c415c96f53221103dd05b1725622d57d
mateofonseca2003 <maso200222@gmail.com>
2026-01-20 11:01:49 -0300

    Fix app export (default) + clean server listen

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,5 @@
 import "dotenv/config";
-import * as appModule from "./app.js";
-
-const app = appModule.default ?? appModule.app;
-
-if (!app) {
-  throw new Error("app.js no exporta default ni export { app }");
-}
+import app from "./app.js";
 
 const PORT = Number(process.env.PORT) || 4000;
 
